b1a463987646f060f328aa09f18867e147844070
David Carr <dave@daveismyname.blog>
2019-02-03 00:44:52 +0000

    added tweak to deal with minus figures.
    When ?=p is less than 0 then default to 1.

--- paginator.php
+++ paginator.php
@@ -78,7 +78,7 @@ class Paginator{
 	*/
 	private function set_instance(){
 		$this->_page = (int) (!isset($_GET[$this->_instance]) ? 1 : $_GET[$this->_instance]); 
-		$this->_page = ($this->_page == 0 ? 1 : $this->_page);
+		$this->_page = ($this->_page == 0 ? 1 : $this->_page < 0 ? 1 : $this->_page);
 	}
 
 	/**
